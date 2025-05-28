b3acafbbe2bf679ad08c88ddcae22d5e5958a3cc
SempreAppassionato <106906047+SempreAppassionato@users.noreply.github.com>
2025-05-28 14:03:50 +1200

    all Flask routes added

--- debate_motions.py
+++ debate_motions.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/flagged')
+def flagged():
+    return render_template('flagged.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/random')
+def random():
+    return render_template('random.html')
+
+@app.route('/user_profile')
+def user_profile():
+    return render_template('user_profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
